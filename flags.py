9b42e0f4be2d38b290b75f9b9fae156d2e08ec39
bradhutc <bradhutch2004@gmail.com>
2023-12-20 11:44:45 -0500

    Update for Curves

--- flags.py
+++ flags.py
@@ -2,8 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-
-file_path = '/N/project/catypGC/BDBS/bdbsparallaxprocessed_data.csv'
+from scipy.interpolate import make_interp_spline
+import numpy as np
+file_path = 'C:/Users/Bradl/OneDrive/Desktop/BDBS/stellaridentification/bdbsparallaxprocessed_data.csv'
 df = pd.read_csv(file_path)[['gaia_id','BDBS_ID','umag','gmag','rmag','imag','zmag','ymag']]
 
 
@@ -13,9 +14,9 @@ bands = ['umag', 'gmag', 'rmag', 'imag', 'zmag', 'ymag']
 def is_anomaly(row):
     for i in range(1, len(bands) - 1):
         # Check for a significant dip compared to the left band
-        if row[bands[i]] < row[bands[i - 1]] and abs(row[bands[i-1]] - row[bands[i]]) >= 0.1:
+        if row[bands[i]] < row[bands[i - 1]] and abs(row[bands[i-1]] - row[bands[i]]) >= 0.25:
             # Check for a significant increase compared to the right band
-            if row[bands[i + 1]] > row[bands[i]] and abs(row[bands[i + 1]] - row[bands[i]]) >= 0.1:
+            if row[bands[i + 1]] > row[bands[i]] and abs(row[bands[i + 1]] - row[bands[i]]) >= 0.25:
                 return 0  # Anomaly
     return 1  # Not an anomaly
 
@@ -29,15 +30,38 @@ print(df.head())
 
 
 sns.set_style("whitegrid")
-stars_df = df[df['label'] == 0]
-print(len(stars_df))
+anom_df = df[df['label'] == 0]
+print(len(anom_df))
 
 plt.figure(figsize=(14, 8))
-plt.plot(bands, stars_df[['umag', 'gmag', 'rmag', 'imag', 'zmag', 'ymag']].T.values, marker='o')
-plt.title('Photometric Pattern of Stars Across Different Bands', fontsize=16)
+spl = make_interp_spline(range(len(bands)), anom_df[['umag', 'gmag', 'rmag', 'imag', 'zmag', 'ymag']].T.values, k=2)
+bands_smooth = np.linspace(0, 5, 300)
+curve_smooth = spl(bands_smooth)
+plt.plot(bands_smooth, curve_smooth)
+plt.title('Photometric Pattern of Anomalous Stars Across Different Bands', fontsize=16)
 plt.xlabel('Photometric Band', fontsize=14)
 plt.ylabel('Magnitude', fontsize=14)
+plt.xticks(range(len(bands)), bands)
 plt.gca().invert_yaxis()
 plt.show()
 
-stars_df.to_csv('anomalies.csv', index=False)
+reg_df = df[df['label'] == 1]
+sample_df=reg_df.sample(5)
+
+plt.figure(figsize=(14, 8))
+spl = make_interp_spline(range(len(bands)), sample_df[['umag', 'gmag', 'rmag', 'imag', 'zmag', 'ymag']].T.values, k=2)
+bands_smooth = np.linspace(0, 5, 300)
+curve_smooth = spl(bands_smooth)
+plt.plot(bands_smooth, curve_smooth)
+plt.title('Photometric Pattern of Non-Anomalous Stars Across Different Bands', fontsize=16)
+plt.xlabel('Photometric Band', fontsize=14)
+plt.ylabel('Magnitude', fontsize=14)
+plt.xticks(range(len(bands)), bands)
+plt.gca().invert_yaxis()
+plt.show()
+
+
+
+
+anom_df.to_csv('anomalies.csv', index=False)
+reg_df.to_csv('clean.csv', index=False)
